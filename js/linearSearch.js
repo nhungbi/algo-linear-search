985e821f3b0b1b283ad6184afa2f6a0509d5a4e0
nhungbi <nhungvan16@gmail.com>
2022-05-24 13:34:29 -0500

    fn

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,26 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    let index = 0
+    for (let elt of arrayToSearchThrough) {
+        if (elt === valueToFind) {
+            return index
+        }
+        index ++
+    }
 
 };
+
+exports.linearSearchGlobal = function (valueToFind, arrayToSearchThrough) {
+    let index = 0
+    let output = []
+    for (let elt of arrayToSearchThrough) {
+        if (elt === valueToFind) {
+            output.push(index)
+        }
+        index ++
+    }
+    return output
+}
+
 
--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,20 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    index = 0
+    for elt in array_to_search_through:
+        if elt == value_to_find:
+            return index
+        index +=1
+
+def global_linear_seach (value_to_find, array_to_search_through):
+    output = []
+    index = 0
+    for elt in array_to_search_through:
+        if elt == value_to_find:
+            output.append(index)
+        index +=1
+    
+    return output
+
+# print(global_linear_seach('a',list('bananas')))
